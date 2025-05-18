b7d6cdca60a7e024742327a9fcae41facda773a3
hideask <song.jia.16@gmail.com>
2025-05-18 20:50:20 +0800

    torch_test

--- LLM/appendix/torch_test.py
+++ LLM/appendix/torch_test.py
@@ -142,3 +142,72 @@ train_loader = DataLoader(
 
 for idx, (x, y) in enumerate(train_loader):
     print(f"Batch {idx + 1}:", x, y)
+
+# a typical training loop
+import torch.nn.functional as F 
+
+torch.manual_seed(123)
+model = NeuralNetwork(num_inputs=2, num_outputs=2)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+
+num_epochs = 3
+for epoch in range(num_epochs):
+    model.train()
+    for batch_idx, (features, labels) in enumerate(train_loader):
+        logits = model(features)
+
+        loss = F.cross_entropy(logits, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        ### LOGGING
+        print(f"Epoch: {epoch+1:03d}/{num_epochs:03d}"
+                f" | Batch {batch_idx:03d}/{len(train_loader):03d}"
+                f" | Train Loss: {loss:.2f}")
+    
+    model.eval()
+    # Insert optional model evaluation code  
+
+model.eval()
+with torch.no_grad():
+    outputs = model(X_train)
+print(outputs)
+
+torch.set_printoptions(sci_mode=False)
+probas = torch.softmax(outputs, dim=1)
+print(probas)
+
+predictions = torch.argmax(probas, dim=1)
+print(predictions)
+
+predictions = torch.argmax(probas, dim=0)
+print(predictions)
+
+predictions = torch.argmax(outputs, dim=1)
+print(predictions)
+
+print(predictions == y_train)
+
+print(torch.sum(predictions == y_train))
+
+def compute_accuracy(model, dataloader):
+    model = model.eval()
+    correct = 0.0
+    total_examples = 0
+
+    for idx, (features, labels) in enumerate(dataloader):
+
+        with torch.no_grad():
+            logits = model(features)
+
+        predictions = torch.argmax(logits, dim = 1)
+        compare = labels == predictions
+        correct += torch.sum(compare)
+        total_examples += len(compare)
+
+    return (correct / total_examples).item()
+
+print(compute_accuracy(model, train_loader))
+print(compute_accuracy(model, test_loader))
